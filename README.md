e215e7749fefe2c74011c3f370601e48e943d25c
Syed Ibrahim <siz998@gmail.com>
2021-10-16 22:20:15 +0530

    added keyboard interrupt and updated README.md

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # GAuthy
+
+---
 Cli based Google Authenticator TOTP Generator
+
+## Prerequisites
+
+---
+1. Requires **Python 3.6+**
+2. Install requirements from _**requirements.txt**_
+```commandline
+pip install -r requirements.txt
+```
+## How to
+
+---
+### To generate TOTP using Authenticator Key
+```commandline
+python gauthy -k YOURAUTHKEYHERE
+python gauthy --key YOURAUTHKEYHERE
+```
+### For help
+```commandline
+ python gauthy --help
+```
--- gauthy/__main__.py
+++ gauthy/__main__.py
@@ -10,22 +10,26 @@ app = typer.Typer(help="CLI tool to generate google authenticator TOTP.")
 
 @app.command()
 def generate(auth_key: str = typer.Option(..., "--key", "-k")):
-    while True:
-        click.clear()
-        totp, validity = GenTotp.gen_totp(auth_key)
-        prev_totp = typer.style(
-            f"{totp.at(for_time=datetime.now() - timedelta(seconds=30))}", fg=typer.colors.RED, dim=True)
-        curr_totp = typer.style(
-            f"{totp.now()}", fg=typer.colors.BRIGHT_GREEN, bold=True)
-        next_totp = typer.style(
-            f"{totp.at(for_time=datetime.now() + timedelta(seconds=30))}", fg=typer.colors.BLUE, dim=True)
-        typer.echo(f"PREV\t\tCURRENT\t\tNEXT")
-        typer.echo(f"{prev_totp}\t\t{curr_totp}\t\t{next_totp}")
-        with typer.progressbar(
-                range(int(validity)), label="Token Validity") as progress:
-            for value in progress:
-                time.sleep(1)
-        time.sleep(0.05)
+    try:
+        while True:
+            click.clear()
+            totp, validity = GenTotp.gen_totp(auth_key)
+            prev_totp = typer.style(
+                f"{totp.at(for_time=datetime.now() - timedelta(seconds=30))}", fg=typer.colors.RED, dim=True)
+            curr_totp = typer.style(
+                f"{totp.now()}", fg=typer.colors.BRIGHT_GREEN, bold=True)
+            next_totp = typer.style(
+                f"{totp.at(for_time=datetime.now() + timedelta(seconds=30))}", fg=typer.colors.BLUE, dim=True)
+            typer.echo(f"PREV\t\tCURRENT\t\tNEXT")
+            typer.echo(f"{prev_totp}\t\t{curr_totp}\t\t{next_totp}")
+            with typer.progressbar(
+                    range(int(validity)), label="Token Validity") as progress:
+                for value in progress:
+                    time.sleep(1)
+            time.sleep(0.05)
+    except KeyboardInterrupt:
+        typer.echo("Goodbye...")
+        exit(0)
 
 
 if __name__ == '__main__':
